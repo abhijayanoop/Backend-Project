f36f684e9b331374ab0d41e8338e566da0a8123f
abhijayanoop-elcarreira <abhijay@elcarreira.com>
2025-09-09 20:40:25 +0530

    added pagination

--- controllers/feed.js
+++ controllers/feed.js
@@ -5,11 +5,23 @@ const Post = require("../models/post");
 const path = require("path");
 
 exports.getPosts = (req, res, next) => {
+  const currentPage = req.query.page || 1;
+  const perPage = 2;
+  let totalItems;
+
   Post.find()
+    .countDocuments()
+    .then((count) => {
+      totalItems = count;
+      return Post.find()
+        .skip((currentPage - 1) * perPage)
+        .limit(perPage);
+    })
     .then((posts) => {
       res.status(200).json({
         message: "Fetched posts successfully",
         posts: posts,
+        totalItems,
       });
     })
     .catch((err) => {
@@ -18,6 +30,20 @@ exports.getPosts = (req, res, next) => {
       }
       next(err);
     });
+
+  // Post.find()
+  //   .then((posts) => {
+  //     res.status(200).json({
+  //       message: "Fetched posts successfully",
+  //       posts: posts,
+  //     });
+  //   })
+  //   .catch((err) => {
+  //     if (!err.statusCode) {
+  //       err.statusCode(500);
+  //     }
+  //     next(err);
+  //   });
 };
 
 exports.createPost = (req, res, next) => {
