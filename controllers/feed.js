1dc4d334e33023ac57fedf04fc7e932370eb419a
abhijayanoop-elcarreira <abhijay@elcarreira.com>
2025-09-09 20:34:22 +0530

    file upload, editing and deleting posts

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,6 +1,8 @@
 const { validationResult } = require("express-validator");
+const fs = require("fs");
 
 const Post = require("../models/post");
+const path = require("path");
 
 exports.getPosts = (req, res, next) => {
   Post.find()
@@ -24,18 +26,21 @@ exports.createPost = (req, res, next) => {
     const error = new Error('"Validation failed. Entered data is incorrect"');
     error.statusCode = 422;
     throw error;
-    // return res.status(422).json({
-    //   message: "Validation failed. Entered data is incorrect",
-    //   errors: errors.array(),
-    // });
   }
+
+  if (!req.file) {
+    const error = new Error("No image provided");
+    error.statusCode = 422;
+    throw error;
+  }
+  const imageUrl = req.file.path;
   const title = req.body.title;
   const content = req.body.content;
 
   const post = new Post({
     title: title,
     content: content,
-    imageUrl: "images/book.jpeg",
+    imageUrl: imageUrl,
     creator: { name: "Abhijay" },
   });
 
@@ -77,4 +82,84 @@ exports.getPost = (req, res, next) => {
       }
       next(err);
     });
+};
+
+exports.updatePost = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty) {
+    const error = new Error('"Validation failed. Entered data is incorrect"');
+    error.statusCode = 422;
+    throw error;
+  }
+
+  const postId = req.params.postID;
+  const title = req.body.title;
+  const content = req.body.content;
+
+  let imageUrl = req.body.image;
+  if (req.file) {
+    imageUrl = req.file.path;
+  }
+  if (!imageUrl) {
+    const error = new Error("No file picked");
+    error.statusCode = 422;
+    throw error;
+  }
+  Post.findById(postId)
+    .then((post) => {
+      if (!post) {
+        const error = new Error("Could not find post");
+        error.statusCode = 404;
+        throw error;
+      }
+      if (imageUrl !== post.imageUrl) {
+        clearImage(post.imageUrl);
+      }
+      post.title = title;
+      post.imageUrl = imageUrl;
+      post.content = content;
+      return post.save();
+    })
+    .then((result) => {
+      return res.status(200).json({
+        message: "Post updated",
+        post: result,
+      });
+    })
+    .catch((err) => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
+};
+
+exports.deletePost = (req, res, next) => {
+  const postId = req.params.postId;
+
+  Post.findById(postId)
+    .then((post) => {
+      if (!post) {
+        const error = new Error("Could not find post");
+        error.statusCode = 404;
+        throw error;
+      }
+      clearImage(post.imageUrl);
+      return Post.findByIdAndDelete(postId);
+    })
+    .then((result) => {
+      console.log(result);
+      res.status(200).json({ message: "Deleted Post" });
+    })
+    .catch((err) => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
+};
+
+const clearImage = (filePath) => {
+  filePath = path.join(__dirname, "..", filePath);
+  fs.unlink(filePath, (err) => console.log(err));
 };
